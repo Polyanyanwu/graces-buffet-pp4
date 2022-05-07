19da2e2c1f45214b6ccb3c3c9edf3a8ad74d12d8
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-05-07 23:00:41 +0000

    fix: Cloudinary settings

--- graces_buffet/settings.py
+++ graces_buffet/settings.py
@@ -163,7 +163,7 @@ CLOUDINARY_STORAGE = {
 
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
