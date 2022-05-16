041dd39a326ef810b5f9c85c7879a7a1819dc64f
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-05-16 22:52:58 +0000

    feat: Add filtering by time of booking to algorithim

--- bookings/models.py
+++ bookings/models.py
@@ -58,9 +58,11 @@ class TablesBooked(models.Model):
     booking_id = models.ForeignKey(Booking, on_delete=models.CASCADE)
     # don't delete a table if it has bookings
     table_id = models.ForeignKey(DiningTable, on_delete=models.PROTECT)
+    table_capacity = models.PositiveSmallIntegerField()
     seats_booked = models.PositiveSmallIntegerField(default=0)
     date_booked = models.DateField(auto_now=True)
     time_booked = models.TimeField(null=True)
+
 
     def __str__(self):
         return f"{self.table_id}"
--- bookings/views.py
+++ bookings/views.py
@@ -1,11 +1,12 @@
+from datetime import timedelta, datetime, date
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-# from django.contrib.auth.decorators import login_required
 from django.db import transaction, IntegrityError
+
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.db.models import Sum, F
+from django.db.models import Sum
 
 from cuisine.models import Cuisine, CuisineChoice
 from general_tables.models import DiningTable, BuffetPeriod
@@ -56,7 +57,8 @@ class MakeBookings(View):
                     time_entered = get_object_or_404(time_entered_qs)
                     # check seats availability
                     tables = self.book_seats(int(request.POST.get('seats')),
-                                             request.POST.get('dinner_date'))
+                                             request.POST.get('dinner_date'),
+                                             time_entered)
                     print("tables returned==", tables, type(tables))
                     if len(tables) == 0:
                         # no seats found on selected date
@@ -77,12 +79,9 @@ class MakeBookings(View):
                                 booking_id=booking.instance,
                                 seats_booked=seat,
                                 table_id=table_item,
-                                time_booked=time_entered.start_time)
+                                time_booked=time_entered.start_time,
+                                table_capacity=table_item.total_seats)
 
-                            # Update the Dining Tables
-                            table_item.used_seats += seat
-                            table_item.save()
-                            
                     # save the cuisine choices
                     cuisine_choices = request.POST.getlist('cuisine_option')
                     if len(cuisine_choices) > 0:
@@ -109,8 +108,8 @@ class MakeBookings(View):
 
         # check availability of the dates
         cuisine_queryset = Cuisine.objects.all()
-        price_queryset = SystemPreference.objects.filter(code="P").values()
-        buffet_price = price_queryset[0]['data']
+        price_queryset = SystemPreference.objects.get(code="P")
+        buffet_price = price_queryset.data
         booking_form = BookingForm(data=request.POST)
         no_bookings = Booking.objects.filter(id=None)
         return render(
@@ -124,7 +123,7 @@ class MakeBookings(View):
             }
         )
 
-    def book_seats(self, seats, day_booked):
+    def book_seats(self, seats, day_booked, start_time):
         """ Check availability of seats and book if found
             return a dictionary of the tables/seats booked
         """
@@ -133,48 +132,83 @@ class MakeBookings(View):
         total_seats_dict = DiningTable.objects.all().aggregate(
                             Sum('total_seats'))
         total_seats = total_seats_dict['total_seats__sum']
+
+        # Get duration of each buffet service D
+
+        duration_qs = SystemPreference.objects.get(code="D")
+        duration = duration_qs.data
+
+        start_period_dt = datetime.combine(date.today(
+            ), start_time.start_time) - timedelta(minutes=duration)
+        start_period = start_period_dt.time()
+        end_period_dt = datetime.combine(date.today(), start_time.start_time)\
+            + timedelta(minutes=duration)
+        end_period = end_period_dt.time()
+
+        day_booked_dt = datetime.strptime(day_booked, "%Y-%m-%d").date()
+
         total_booked_on_day = TablesBooked.objects.filter(
-                               date_booked=day_booked).aggregate(
-                               Sum('seats_booked'))
-        seats_already_booked = total_booked_on_day['seats_booked__sum']\
-            if total_booked_on_day['seats_booked__sum'] else 0
+            time_booked__gte=start_period,
+            time_booked__lte=end_period,
+            date_booked=day_booked_dt).aggregate(
+            sum_booked=Sum('seats_booked'))
+
+        seats_already_booked = total_booked_on_day['sum_booked']\
+            if total_booked_on_day['sum_booked'] else 0
         available = total_seats - seats_already_booked
+
+        # check which tables have vacant seats by getting their
+        # installed capacity from DiningTables - used seats from TablesBooked
+        booked_tabs_qs = TablesBooked.objects.filter(
+            date_booked=day_booked_dt,
+            time_booked__gte=start_period,
+            time_booked__lte=end_period)\
+            .values('table_id', 'table_capacity').order_by('table_id')\
+            .annotate(total_seat=Sum('seats_booked'))
+
+        # dictionary of table_id, table capacity and total seats used
+        booked_tables = {}
+        for item in booked_tabs_qs:
+            booked_tables[item['table_id']] = [item['table_capacity'],
+                                               item['total_seat']]
+        tables = DiningTable.objects.all()
+        tables_available = {}
+        for table in tables:
+            t_id = table.id
+            if booked_tables.get(t_id):
+                if booked_tables[t_id][0] - booked_tables[t_id][1] > 0:
+                    tables_available[table] = booked_tables[0] - booked_tables[1] 
+            else:
+                tables_available[table] = table.total_seats
+        sort_tables_available = dict(sorted(tables_available.items(),
+                                            key=lambda x: x[1], reverse=True))
         if seats > available:
             return booked
         print(total_seats, total_booked_on_day, available)
-        # find the tables to use for the booking
 
-        # din_tables_below = DiningTable.objects.annotate(
-        #     diff=F('total_seats')
-        #     - F('used_seats')).filter(diff__lte=seats).order_by('diff')
-
-        dining_tables = DiningTable.objects.annotate(
-            diff=F('total_seats')
-            - F('used_seats')).order_by('-diff')
-        print("Dining Tables", dining_tables)
-        for table_item in dining_tables:
+        for table_a, seats_a in sort_tables_available.items():
             # check to see if you get an exact table matching the seats needed
-            if table_item.seats_remaining() == seats:
+            if seats_a == seats:
                 booked.clear()
-                booked[table_item] = table_item.seats_remaining()
+                booked[table_a] = seats_a
                 return booked
             # check to see if you get a table having enough seats to match need
-            # used the minimum seats possible
-            elif table_item.seats_remaining() > seats:
+            # keep replacing the higher capacity seats till the least one
+            elif seats_a > seats:
                 booked.clear()
-                booked[table_item] = table_item.seats_remaining()
+                booked[table_a] = seats_a
 
         # if after checking we can't get a table with enough seats
         # comnine tables with largest space until need is met
         allocated = 0
         if len(booked) == 0:
-            for table_item in dining_tables:
-                remaining = table_item.seats_remaining()
-                if seats - allocated <= remaining:
-                    remaining = seats - allocated
-                booked[table_item] = remaining
-                allocated += remaining
-                if allocated == seats:
-                    break
-        print(booked)
+            for table_a, seats_a in sort_tables_available.items():
+                remaining = seats_a
+                if remaining > 0:
+                    if seats - allocated <= remaining:
+                        remaining = seats - allocated
+                    booked[table_a] = remaining
+                    allocated += remaining
+                    if allocated == seats:
+                        break
         return booked
