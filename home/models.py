2e9cd14535b0d48f3cdb7f319e826f3cdb5cf266
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-05-22 13:33:59 +0000

    feat: Add contact model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+""" Models for Contact message """
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Contact(models.Model):
+    """ Data model for contact us """
+
+    options = [
+            ('Information', 'Information'),
+            ('Cuisine', 'Cuisine'),
+            ('Seating', 'Seating'),
+            ('Others', 'Others'),
+        ]
+
+    subject = models.CharField(choices=options, default='Information',
+                               max_length=200, null=False, blank=False)
+    message_body = models.TextField(null=False, blank=False)
+    sender = models.EmailField(max_length=200, null=False, blank=False)
+    date_time = models.DateTimeField(auto_now_add=True)
+    # user could be guest
+    user = models.ForeignKey(User, on_delete=models.SET_NULL,
+                             null=True, blank=True)
+
+    def __str__(self):
+        return str(self.sender)
